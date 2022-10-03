220408f887800ebfa571c2fcf6f17f753f8675de
EfimovE <efimov_e@mail.ru>
2022-10-03 20:41:53 +0500

    save5

---
+++ Task5.py
@@ -0,0 +1,8 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+
+list = ['AbcAbc', 456787494, 589.4524, 'Test', 123, 0.15]
+print(list) 
+random.shuffle(list)
+print('Список, после перемешивания:', list)
